cb40365a08e827634931bc15338928e1710fa60f
Oglosvald <briandave@hotmail.com>
2023-06-06 09:26:16 -0300

    Components from list

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Formulary from './components/Formulary';
+import List from './components/List';
 function App() {
   return (
       <div className="App">
         <Formulary/>
+          <List/>
       </div>
   );
 }
